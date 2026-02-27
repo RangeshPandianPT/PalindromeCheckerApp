6c72685aa16e92eb4ead5bb3747c616bcc6f5728
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2026-02-27 09:11:29 +0530

    Refactor PalindromeCheckerApp structure

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -3,20 +3,16 @@
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-
-        // Hardcoded string
         String input = "madam";
         String reversed = "";
-
-        // Loop only till half of the string length
+        
         for (int i = input.length() - 1; i>=0; i--) {
             reversed += input.charAt(i);
         }
 
         System.out.println("Original String" + input);
         System.out.println("Reversed String " + reversed);
-        // Output result
-
+        
         if (input.equals(reversed)) {
             System.out.println("Result: The String is a Palindrome.");
         }
@@ -24,4 +20,5 @@ public class PalindromeCheckerApp {
             System.out.println("Result: The String is not a Palindrome");
         }
     }
+
 }
